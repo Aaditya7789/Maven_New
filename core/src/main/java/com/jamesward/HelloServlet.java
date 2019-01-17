2203ac77f8d95f48e296e5cb87d75eaa8764e1ed
tom <tom@example.com>
2019-01-17 17:48:44 +0000

    Chnage in Hello

--- core/src/main/java/com/jamesward/HelloServlet.java
+++ core/src/main/java/com/jamesward/HelloServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 @WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
 public class HelloServlet extends HttpServlet {
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
